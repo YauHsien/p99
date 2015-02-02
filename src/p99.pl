73a37830cd8252e52e0512e62887548425ec1a49
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2015-02-03 00:22:19 +0800

    Problems solved until 1.05.

--- src/p99.pl
+++ src/p99.pl
@@ -1,11 +1,36 @@
 :- module(p99, ['1.01'/2, my_last/2,
-                '1.02']).
+                '1.02'/2, my_last_but_one/2,
+	        '1.03'/3, element_at/3,
+                '1.04'/2, list_len/2
+	  ]).
 
 % 1.01 (*) Find the last element of a list.
-% Example:
-% ?- my_last(X,[a,b,c,d]).
-% X = d
+%       Example:
+%       ?- my_last(X,[a,b,c,d]).
+%       X = d
 my_last(A, B) :- '1.01'(A, B).
 '1.01'(X, [X]) :- !.
 '1.01'(X, [_|Ys]) :- '1.01'(X, Ys).
+
+% 1.02 (*) Find the last but one element of a list.
+%       (de: zweitletztes Element, fr: avant-dernier élément)
+my_last_but_one(A, B) :- '1.02'(A, B).
+'1.02'(X, [X, _]) :- !.
+'1.02'(X, [_|Ys]) :- '1.02'(X, Ys).
+
+% 1.03 (*) Find the Kth element of a list.
+%       The first element in the list is number 1.
+%       Example:
+%       ?- element_at(X,[a,b,c,d,e],3).
+%       X = c
+element_at(X, List, N) :- '1.03'(X, List, N).
+'1.03'(X, [X|_], 1) :- !.
+'1.03'(X, [_|List], N) :-
+    N1 is N - 1,
+    '1.03'(X, List, N1).
+
+% 1.04 (*) Find the number of elements of a list.
+list_len(List, N) :- '1.04'(List, N).
+'1.04'([], 0).
+'1.04'([_|List], N) :- '1.04'(List, N1), N is N1 + 1.
 
--- src/unit-test.pl
+++ src/unit-test.pl
@@ -9,17 +9,29 @@
 %   p99:1.01(b,[a,b]) passed.
 %   p99:1.01(a,[a,b]) did not pass.
 
-case([ p99:'1.01'(b, [a,b]),
-       p99:'1.01'(a, [a,b])  ]).
+case([     p99:'1.01'(b, [a,b]),
+       not(p99:'1.01'(a, [a,b])),
+           p99:'1.02'(b, [a,b,c]),
+       not(p99:'1.02'(a, [a,b,c])),
+       not(p99:'1.02'(c, [a,b,c])),
+       not(p99:'1.02'(a, [a])),
+	   p99:'1.03'(a, [a,b,c], 1),
+           p99:'1.03'(c, [a,b,c], 3),
+       not(p99:'1.03'(a, [], 1)),
+	   p99:'1.04'([], 0),
+	   p99:'1.04'([a,b], 2),
+       not(p99:'1.04'([a,b,c,d,e], 6))
+  ]).
 
 unit_test :-
     case(List),
+    format('Unit test start.~n'),
     assert_all(List),
     halt.
 
 assert_all([]) :-
-    format('All cases passed.~n').
-assert_all([G|List]) :- callable(G), G, !,
+    format('Unit test done.~n').
+assert_all([G|List]) :- G, !,
     format('~p passed.~n', [G]),
     assert_all(List).
 assert_all([G|_]) :-
