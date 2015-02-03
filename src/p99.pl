7b5cfb5fdf5c5997b75aed8949fcc7d3fa9335f9
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2015-02-03 12:30:33 +0800

    Problem solved.

--- src/p99.pl
+++ src/p99.pl
@@ -1,7 +1,13 @@
 :- module(p99, ['1.01'/2, my_last/2,
                 '1.02'/2, my_last_but_one/2,
 	        '1.03'/3, element_at/3,
-                '1.04'/2, list_len/2
+                '1.04'/2, list_len/2,
+		'1.05'/2, list_rev/2,
+		'1.06'/1, palindome/1,
+                '1.07'/2, my_flatten/2,
+		'1.08'/2, compress/2,
+		'1.09'/2, pack/2,
+		'1.10'/2, encode/2
 	  ]).
 
 % 1.01 (*) Find the last element of a list.
@@ -33,4 +39,90 @@ element_at(X, List, N) :- '1.03'(X, List, N).
 list_len(List, N) :- '1.04'(List, N).
 '1.04'([], 0).
 '1.04'([_|List], N) :- '1.04'(List, N1), N is N1 + 1.
+
+% 1.05 (*) Reverse a list.
+list_rev(List, List1) :- '1.05'(List, List1).
+'1.05'([], []).
+'1.05'([X], [X]) :- !.
+'1.05'([X|List], List1) :-
+    '1.05'(List, List2),
+    list:append(List2, [X], List1).
+% I tried writing the third clause of '1.05' as followings,
+%   '1.05'([X|List], List1) :-
+%       '1.05'(List, List2),
+%       list_append(List2, [X], List1).
+%   list_append(List1, List2, List3) :-
+%       list_rev(List1, List4),
+%       list_append(List1, List2, List4, List3).
+%   list_append(_, [], Acc, List3) :- format('a~n'),
+%       list_rev(Acc, List3).
+%   list_append(_, [X|List], Acc, List3) :- format('b~n'),
+%       list_append(_, List, [X|Acc], List3).
+% and I found that it is circular recursion. Interesting.
+
+% 1.06 (*) Find out whether a list is a palindrome.
+%       A palindrome can be read forward or backward; e.g. [x,a,m,a,x].
+palindrome(List) :- '1.06'(List).
+'1.06'(List) :-
+    list_rev(List, List).
+
+% 1.07 (**) Flatten a nested list structure.
+%        Transform a list, possibly holding lists as elements into a 'flat' list by replacing each list with its elements (recursively).
+%
+% Example:
+% ?- my_flatten([a, [b, [c, d], e]], X).
+% X = [a, b, c, d, e]
+%
+% Hint: Use the predefined predicates is_list/1 and append/3
+my_flatten(List, List1) :- '1.07'(List, List1).
+'1.07'([], []).
+'1.07'([X|List], List1) :- is_list(X), !,
+    '1.07'(X, X1),
+    '1.07'(List, List2),
+    list:append(X1, List2, List1).
+'1.07'([X|List], [X|List1]) :-
+    '1.07'(List, List1).
+
+% 1.08 (**) Eliminate consecutive duplicates of list elements.
+%       If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+%
+% Example:
+% ?- compress([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+% X = [a,b,c,a,d,e]
+compress(List, List1) :- '1.08'(List, List1).
+'1.08'(List, List1) :-
+    '1.08'(List, [], List1).
+'1.08'([], Acc, List1) :-
+    list_rev(Acc, List1).
+'1.08'([X|List], [X|Acc], List1) :- !,
+    '1.08'(List, [X|Acc], List1).
+'1.08'([X|List], Acc, List1) :-
+    '1.08'(List, [X|Acc], List1).
+
+% 1.09 (**) Pack consecutive duplicates of list elements into sublists.
+%       If a list contains repeated elements they should be placed in separate sublists.
+%
+% Example:
+% ?- pack([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+% X = [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]
+pack(List, List1) :- '1.09'(List, List1).
+'1.09'([], []).
+'1.09'([X,X|List], [[X|Xs]|List1]) :- !,
+    '1.09'([X|List], [Xs|List1]).
+'1.09'([X|List], [[X]|List1]) :-
+    '1.09'(List, List1).
+
+% 1.10 (*) Run-length encoding of a list.
+% Use the result of problem 1.09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as terms [N,E] where N is the number of duplicates of the element E.
+%
+% Example:
+% ?- encode([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+% X = [[4,a],[1,b],[2,c],[2,a],[1,d][4,e]]
+encode(List, List1) :- '1.10'(List, List1).
+'1.10'([], []).
+'1.10'([X,X|List], [[N,X]|List1]) :- !,
+    '1.10'([X|List], [[N1,X]|List1]),
+    N is N1 + 1.
+'1.10'([X|List], [[1,X]|List1]) :-
+    '1.10'(List, List1).
 
--- src/unit-test.pl
+++ src/unit-test.pl
@@ -20,17 +20,26 @@ case([     p99:'1.01'(b, [a,b]),
        not(p99:'1.03'(a, [], 1)),
 	   p99:'1.04'([], 0),
 	   p99:'1.04'([a,b], 2),
-       not(p99:'1.04'([a,b,c,d,e], 6))
+       not(p99:'1.04'([a,b,c,d,e], 6)),
+           p99:'1.05'([], []),
+	   p99:'1.05'([a,b,c,d], [d,c,b,a]),
+	   p99:'1.06'([]),
+	   p99:'1.06'([a]),
+       not(p99:'1.06'([a,b,c])),
+           p99:'1.07'([a, [b, [c, d], e]], [a,b,c,d,e]),
+           p99:'1.08'([a,a,a,a,b,c,c,a,a,d,e,e,e,e], [a,b,c,a,d,e]),
+	   p99:'1.09'([a,a,a,a,b,c,c,a,a,d,e,e,e,e], [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]),
+           p99:'1.10'([a,a,a,a,b,c,c,a,a,d,e,e,e,e], [[4,a],[1,b],[2,c],[2,a],[1,d],[4,e]])
   ]).
 
 unit_test :-
     case(List),
     format('Unit test start.~n'),
-    assert_all(List),
-    halt.
+    assert_all(List).
 
 assert_all([]) :-
-    format('Unit test done.~n').
+    format('Unit test done.~n'),
+    halt.
 assert_all([G|List]) :- G, !,
     format('~p passed.~n', [G]),
     assert_all(List).
